c1f6ace8e8992f3cca63bf1658c81831ce14e458
fr8app <84604664+fr8app@users.noreply.github.com>
2024-07-18 13:15:33 +0530

    Update server.js
    changed auth token

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const accountSid = 'ACac793f02cf5fd252f8206d87bb06d91a';
-const authToken = '287c05680510f3515939b3b79eb0be97';
+const authToken = '5f3f80cea5774a15530e44d1f77f5b5c';
 const client = new twilio(accountSid, authToken);
 
 const employees = [
